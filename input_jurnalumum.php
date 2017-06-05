b24ec90c10353fac29a1787c62054c2ad0da1001
Lintang Timur <lintangtimur@protonmail.com>
2017-06-06 01:14:21 +0700

    finish

---
+++ input_jurnalumum.php
@@ -0,0 +1,19 @@
+<?php
+require "core/database/Connection.php";
+header('Content-Type: application/json');
+
+$con = Connection::Connect();
+$tanggal = $_POST['tglTransaksi'];
+$bukti = $_POST['bukti'];
+$tipeKas = $_POST['tipeKas'];
+$unit = $_POST['nounit'];
+$user = "";
+
+$sql = "INSERT INTO jurnalumum VALUES('', '$tanggal', '', '$bukti', '$tipeKas', '$user', '$unit','0', '0', '0','0','0','0')";
+$result = $con->query($sql);
+if ($result) {
+    $data = ["success" => true];
+} else {
+    $data = ["success" => false];
+}
+echo json_encode($data);
---
+++ inputkredit.php
@@ -0,0 +1,54 @@
+<?php
+require "core/database/Connection.php";
+require "core/utilities/Currency.php";
+header('Content-Type: application/json');
+$con = Connection::Connect();
+
+$inputakun = $_POST['inputakun'];
+$jumlah = $_POST['jumlahinput'];
+$bukti = $_POST['bukti'];
+$jenis = $_POST['tipe'];
+$unit = $_POST['nounit'];
+$nocek = $_POST['nocek'];
+$keterangan = $_POST['keterangan'];
+
+$sql_insert = "INSERT INTO jurnaldetil
+ VALUES('','$bukti', '$inputakun', '$unit', '$jenis', '$jumlah', '$keterangan', '$nocek', '0')";
+ $result = $con->query($sql_insert);
+
+$sql_select = "SELECT jurnaldetil.nodetiljurnal,jurnaldetil.noakun, kodeunit.nama,jurnaldetil.tipe,  jurnaldetil.jumlah,jurnaldetil.keterangan
+  FROM jurnaldetil
+  JOIN kodeunit ON jurnaldetil.unit = kodeunit.unit
+  WHERE jurnaldetil.nobukti = '$bukti'
+  AND jurnaldetil.del = '0'";
+
+$result = $con->query($sql_select);
+$table = "<tr>";
+$looping = 0;
+while ($row = $result->fetch_object()) {
+    $looping++;
+    $table .= "<td>$looping"."</td>";
+    $table .= "<td>$row->noakun"."</td>";
+    $table .= "<td>$row->nama"."</td>";
+    $nominal = Currency::rupiah($row->jumlah);
+    if ($row->tipe == "D") {
+        $table .= "<td>$nominal</td><td></td>";
+    } else {
+        $table .= "<td></td><td>$nominal</td>";
+    }
+    $table .= "<td>$row->keterangan"."</td>";
+    $table .= "<td><button class='btn red darken-4 waves-effect waves-light' name=''>Del<i class='material-icons right'>delete_forever</i></button></td></tr>";
+}
+$data['table'] = $table;
+
+$sql_sum = "SELECT SUM(jumlah) AS total
+  FROM jurnaldetil
+  WHERE nobukti = '$bukti'
+  AND tipe = '$jenis'
+  AND del = '0'";
+
+$result = $con->query($sql_sum);
+while ($row = $result->fetch_object()) {
+    $data['total'] = Currency::rupiah($row->total);
+}
+echo json_encode($data);
