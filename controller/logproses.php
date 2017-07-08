9232d19f044b2fb90aaecc37fba9056df85d71bb
Lintang Timur <lintangtimur@protonmail.com>
2017-07-08 21:39:53 +0700

    add login

---
+++ controller/logproses.php
@@ -0,0 +1,24 @@
+<?php
+session_start();
+/*
+* username : sandbox
+* password : centos97
+*/
+$con = Connection::Connect();
+$qb = new QueryBuilder();
+
+$username = $_POST['username'];
+$password = hash('sha256', $_POST['password']);
+
+$qb->select('*')
+->from('pengguna')
+->where("username = '$username'")
+->whereAnd("password = '$password'");
+
+$result = $con->query(
+  $qb->result()
+);
+if ($result->num_rows == 1) {
+    $_SESSION['login'] = $username;
+}
+header("Location: /");
