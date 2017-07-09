5958d0dd68244c74957b4cb2a89fc6daea2d95ae
Lintang Timur <lintangtimur@protonmail.com>
2017-07-09 21:25:04 +0700

    tampilan about revisi

--- view/about.view.php
+++ view/about.view.php
@@ -3,8 +3,54 @@
   <head>
     <meta charset="utf-8">
     <title>About</title>
+    <link rel="stylesheet" href="asset/css/materialize.min.css">
+    <link rel="stylesheet" href="asset/css/main.css">
+    <link rel="stylesheet" href="https:/fonts.googleapis.com/icon?family=Material+Icons">
   </head>
   <body>
+    <div class="section">
     <h1>This is About</h1>
+  <div class="row">
+    <div class="col s4">
+      <div class="card blue-grey darken-1">
+        <div class="card-image">
+              <img src="../asset/img/github.png">
+        </div>
+        <div class="card-content white-text">
+          Daniel Aditama
+        </div>
+        <div class="card-action">
+              <a href="#">15.N1.0015</a>
+        </div>
+      </div>
+    </div>
+    <div class="col s4">
+      <div class="card blue-grey darken-1">
+        <div class="card-image">
+              <img src="../asset/img/github.png">
+        </div>
+        <div class="card-content white-text">
+          St Lintang Timur
+        </div>
+        <div class="card-action">
+              <a href="#">15.N1.0020</a>
+        </div>
+      </div>
+    </div>
+    <div class="col s4">
+      <div class="card blue-grey darken-1">
+        <div class="card-image">
+              <img src="../asset/img/github.png">
+        </div>
+        <div class="card-content white-text">
+          Gregorius Alvin
+        </div>
+        <div class="card-action">
+              <a href="#">15.N2.0002</a>
+        </div>
+      </div>
+    </div>
+  </div>
+  </div>
   </body>
 </html>
