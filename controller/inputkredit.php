ae3ddf5cc28a0205e195beb4b48e729d47b80351
Lintang Timur <lintangtimur@protonmail.com>
2017-08-24 23:51:36 +0700

    fix inputdebet, inputkredit in index

--- controller/inputkredit.php
+++ controller/inputkredit.php
@@ -12,9 +12,15 @@ $unit = $_POST['nounit'];
 $nocek = $_POST['nocek'];
 $keterangan = $_POST['keterangan'];
 
-$sql_insert = "INSERT INTO jurnaldetil
- VALUES('','$bukti', '$inputakun', '$unit', '$jenis', '$jumlah', '$keterangan', '$nocek', '0')";
- $result = $con->query($sql_insert);
+// $sql_insert = "INSERT INTO jurnaldetil
+//  VALUES('','$bukti', '$inputakun', '$unit', '$jenis', '$jumlah', '$keterangan', '$nocek', '0')";
+$insert_pdo = "INSERT INTO public.jurnaldetil(
+ nobukti, noakun, unit, tipe, jumlah, keterangan, nocek, del)
+ VALUES (?, ?, ?, ?, ?, ?, ?, 0)";
+$result = $con->prepare($insert_pdo);
+$result->execute([
+  $bukti, $inputakun, $unit, $jenis, $jumlah, $keterangan, $nocek
+]);
 
 $sql_select = "SELECT jurnaldetil.nodetiljurnal,jurnaldetil.noakun, kodeunit.nama,jurnaldetil.tipe,  jurnaldetil.jumlah,jurnaldetil.keterangan
   FROM jurnaldetil
@@ -25,18 +31,18 @@ $sql_select = "SELECT jurnaldetil.nodetiljurnal,jurnaldetil.noakun, kodeunit.nam
 $result = $con->query($sql_select);
 $table = "<tr>";
 $looping = 0;
-while ($row = $result->fetch_object()) {
+while ($row = $result->fetch()) {
     $looping++;
     $table .= "<td>$looping"."</td>";
-    $table .= "<td>$row->noakun"."</td>";
-    $table .= "<td>$row->nama"."</td>";
-    $nominal = Currency::rupiah($row->jumlah);
-    if ($row->tipe == "D") {
+    $table .= "<td>".$row['noakun']."</td>";
+    $table .= "<td>".$row['nama']."</td>";
+    $nominal = Currency::rupiah($row['jumlah']);
+    if ($row['tipe'] == "D") {
         $table .= "<td>$nominal</td><td></td>";
     } else {
         $table .= "<td></td><td>$nominal</td>";
     }
-    $table .= "<td>$row->keterangan"."</td>";
+    $table .= "<td>".$row['keterangan']."</td>";
     $table .= "<td><button class='btn red darken-4 waves-effect waves-light' name=''>Del<i class='material-icons right'>delete_forever</i></button></td></tr>";
 }
 $data['table'] = $table;
@@ -48,7 +54,7 @@ $sql_sum = "SELECT SUM(jumlah) AS total
   AND del = '0'";
 
 $result = $con->query($sql_sum);
-while ($row = $result->fetch_object()) {
-    $data['total'] = Currency::rupiah($row->total);
+while ($row = $result->fetch()) {
+    $data['total'] = Currency::rupiah($row['total']);
 }
 echo json_encode($data);
